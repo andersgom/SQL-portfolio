e5054b95404333cd24fb8ebe75ffb7a2ba2fa7a5
Andrés Sánchez Gómez <72261391+andersgom@users.noreply.github.com>
2022-02-19 13:33:12 +0000

    Add SQL script.

---
+++ SQL Scripts - Data cleaning.sql
@@ -0,0 +1,8 @@
+-- CREATING THE DATABASE
+
+DROP DATABASE IF EXISTS `sql_datacleaning`;
+CREATE DATABASE `sql_datacleaning`; 
+USE `sql_datacleaning`;
+
+SET NAMES utf8 ;
+SET character_set_client = utf8mb4 ;
