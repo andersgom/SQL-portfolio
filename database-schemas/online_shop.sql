1e4ce9e0e5982fb3bc136e4a96ce20794c896c94
andersgom <asgomez95@gmail.com>
2022-03-01 12:44:58 +0000

    Change column names

--- database-schemas/online_shop.sql
+++ database-schemas/online_shop.sql
@@ -4,33 +4,33 @@ CREATE SCHEMA `online_shop` DEFAULT CHARACTER SET utf8mb4 ;
 USE `online_shop`;
 
 CREATE TABLE `online_shop`.`transactions` (
-  `transaction-id` VARCHAR(15) NOT NULL,
-  `product-id` VARCHAR(15) NOT NULL,
-  `customer-id` VARCHAR(15) NOT NULL,
+  `transaction_id` VARCHAR(15) NOT NULL,
+  `product_id` VARCHAR(15) NOT NULL,
+  `customer_id` VARCHAR(15) NOT NULL,
   `date` DATE NOT NULL,
   `price` INT NULL,
   `quantity` INT NULL,
-  PRIMARY KEY (`transaction-id`, `product-id`, `customer-id`))
+  PRIMARY KEY (`transaction_id`, `product_id`, `customer_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `online_shop`.`products` (
-	`product-id` VARCHAR(15) NOT NULL,
+	`product_id` VARCHAR(15) NOT NULL,
     `product` VARCHAR(60) NOT NULL,
     `price` INT NULL,
     `discounts` INT NULL,
     `stock` INT NULL DEFAULT 0,
-    PRIMARY KEY (`product-id`))
+    PRIMARY KEY (`product_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `online_shop`.`customers` (
-	`customer-id` VARCHAR(15) NOT NULL,
-    `customer-name` VARCHAR(60) NOT NULL,
+	`customer_id` VARCHAR(15) NOT NULL,
+    `customer_name` VARCHAR(60) NOT NULL,
     `country` VARCHAR(60) NULL,
     `address` VARCHAR(60) NULL,
     `email` VARCHAR(60) NOT NULL,
-    `phone-number` VARCHAR(60) NULL,
-    PRIMARY KEY (`customer-id`))
+    `phone_number` VARCHAR(60) NULL,
+    PRIMARY KEY (`customer_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
